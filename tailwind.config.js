45bb0a58f294f86c2b910f2467fc15dfb9b94c31
AYD-7 <ayodejiaronimo@gmail.com>
2025-01-10 14:07:15 +0100

    My first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust the path based on your project structure
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
